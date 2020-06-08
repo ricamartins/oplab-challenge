c74bbc94717109e0ebc25abb50240f222ddde670
Ricardo Martins <martinsc.ricardo@gmail.com>
2020-06-08 19:21:00 -0300

    implement linkedList remove and print methods

--- js/linkedList.js
+++ js/linkedList.js
@@ -80,6 +80,89 @@ class LinkedList {
             return -1;
         }
     }
+
+    remove (value, all=false) {
+
+        if (all == 'all') {
+            let indexes = this.indexOf(value, 'all');
+
+            if (indexes == -1) {
+                return;
+            } else {
+                
+                let i = 0
+                let index = 1
+
+                if (indexes[i] == 0) {
+                    this.head = this.head.next;
+                    this.size--;
+                    i++;
+                    index++;
+                }
+                
+                let current = this.head.next;
+                let previous = this.head;
+
+                while (i < indexes.length) {
+
+                    while (index <= indexes[i]) {
+                        if (index == indexes[i]) {
+                            previous.next = current.next;
+                            current = current.next;
+                            this.size--;
+                        } else {
+                            previous = current;
+                            current = current.next;
+                        }
+
+                        index++;
+                    }
+
+                    i++;
+                }
+            }
+
+        } else {
+            let index = this.indexOf(value);
+
+            if (index == -1) {
+                return;
+            } else if (index == 0) {
+                this.head = this.head.next;
+                this.size--;
+            } else {
+                let current = this.head.next;
+                let previous = this.head;
+                let i = 1;
+
+                while (i <= index) {
+                    if (i == index) {
+                        previous.next = current.next;
+                        this.size--;
+                        return;
+                    }
+                    previous = current;
+                    current = current.next;
+                    i++;
+                }
+            }
+        }
+    }
+
+    print () {
+        let current = this.head;
+        let values = [];
+        let strList = '';
+
+        while (current) {
+            values.push(current.value);
+            current = current.next;
+        }
+
+        strList = '[' + values.join(', ') + ']';
+        console.log(strList);
+        return strList;
+    } 
 }
 
 module.exports = {LinkedList, ListNode}
